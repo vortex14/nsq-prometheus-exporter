39a4706b0ae05c580a9733eee6e58120d26dfcbe
Dmitry Viharev <dviharev@gotrg.com>
2021-06-11 07:28:38 +0300

    port arg

--- main.go
+++ main.go
@@ -15,6 +15,7 @@ import (
 var (
 	nsqLookupdAddress = flag.String("nsq.lookupd.address", "127.0.0.1:4161", "nsqllookupd address list with comma")
 	k8s       = flag.String("nsq.k8s.mode", "", "k8s mode")
+	port = flag.String("port", ":9527", "port usage")
 	k8sMode = false
 )
 
@@ -40,6 +41,8 @@ func main() {
 	http.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request){
 		fmt.Fprint(w, "pong")
 	})
-	log.Fatal(http.ListenAndServe(":9527", nil))
+
+	fmt.Printf("Listen %s", *port)
+	log.Fatal(http.ListenAndServe(*port, nil))
 
 }
